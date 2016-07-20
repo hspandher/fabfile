ac15256e3cd8a5f46209f77069acd06667ac2585
hspandher <hspandher@outlook.com>
2016-07-20 13:44:59 +0530

    define settings

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+from .local_config import *
+
+QA_CODE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'deploy_dir/shine')
+QA_BRANCH_NAME = 'quality_assurance'
---
+++ deploy.py
@@ -0,0 +1,10 @@
+from .base import BranchMergeDeployment
+from . import config
+
+def qa_deploy(issue_branch_hint):
+    BranchMergeDeployment(
+        code_directory = config.QA_CODE_DIRECTORY,
+        scm_url = config.SCM_URL,
+        scm_branch = config.QA_BRANCH_NAME,
+        other_branch_hint = issue_branch_hint
+    ).start()
