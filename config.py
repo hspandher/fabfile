3c729e6a739c9287fbd12b4977b37c943b5f1946
hspandher <hspandher@outlook.com>
2016-07-22 15:25:56 +0530

    Change deployment directory to parent directory in config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 from .local_config import *
 
-QA_CODE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'deploy_dir/shine')
+QA_CODE_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'deploy_dir/shine')
 QA_BRANCH_NAME = 'quality_assurance'
