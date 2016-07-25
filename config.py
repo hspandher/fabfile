30cb9b86b6416ec7c99a664811c4c2bdca91fa3c
hspandher <hspandher@outlook.com>
2016-07-25 12:59:06 +0530

    Add deployment command for staging

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ import os
 
 from .local_config import *
 
-QA_CODE_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'deploy_dir/shine')
+QA_CODE_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'deploy_dir/qa_shine')
 QA_BRANCH_NAME = 'quality_assurance'
+
+STAGING_CODE_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'deploy_dir/staging_shine')
+STAGING_BRANCH_NAME = 'staging'
--- deploy.py
+++ deploy.py
@@ -8,3 +8,11 @@ def qa_deploy(issue_branch_hint):
         scm_branch = config.QA_BRANCH_NAME,
         other_branch_hint = issue_branch_hint
     ).start()
+
+def staging_deploy(issue_branch_hint):
+    BranchMergeDeployment(
+        code_directory = config.STAGING_CODE_DIRECTORY,
+        scm_url = config.SCM_URL,
+        scm_branch = config.STAGING_BRANCH_NAME,
+        other_branch_hint = issue_branch_hint
+    ).start()
