3cc150d33f0f6a1baa916c732933a9c0dff0dbcf
hspandher <hspandher@outlook.com>
2016-07-28 15:41:46 +0530

    delete extras tests

--- config/common.py
+++ config/common.py
@@ -8,3 +8,7 @@ QA_BRANCH_NAME = 'quality_assurance'
 
 STAGING_CODE_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'deploy_dir/staging_shine')
 STAGING_BRANCH_NAME = 'staging'
+
+EMAIL_HOST = '172.22.65.145'
+EMAIL_PORT = 25
+SERVER_EMAIL = 'Shine Deployment <noreply@noone.com>'
---
+++ extras.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from . import config
+
+def send_mail(to_address, subject, message, from_address = config.SERVER_EMAIL, is_html = False):
+    smtp_server = smtplib.SMTP(host = config.EMAIL_HOST, port = config.EMAIL_PORT)
+
+    body = MIMEMultipart()
+
+    body['From'] = from_address
+    body['To'] = to_address
+    body['Subject'] = subject
+
+    message_type = 'text/html' if is_html else 'plain'
+
+    body.attach(MIMEText(message, message_type))
+
+    import ipdb; ipdb.set_trace()
+    with smtplib.SMTP(host = config.EMAIL_HOST, port = config.EMAIL_PORT) as smtp_server:
+        return smtp_server.sendmail(from_address, to_address, body.as_string())
